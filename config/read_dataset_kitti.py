9a877b96e62103d1ea14e9c43c9425c75617686c
Nilai Sallent <nilaisr@gmail.com>
2019-03-09 18:58:56 +0100

    MOD   kitti read

--- config/read_dataset_kitti.py
+++ config/read_dataset_kitti.py
@@ -4,9 +4,9 @@ dataset_dir = '/home/mcv/datasets/M5/classification/KITTI/'
 
 f = open(dataset_dir+'train_images.txt')
 
-for img in listdir(dataset_dir + 'train/'):
-    f.write(img+'\n')
-
+for clas in listdir(dataset_dir + 'train/'):
+    for img in listdir(dataset_dir + 'train/' + clas):
+        f.write(img+'\n')
 
 f.close()
 
@@ -14,8 +14,8 @@ f.close()
 f = open(dataset_dir+'valid_images.txt')
 
 for img in listdir(dataset_dir + 'valid/'):
-    f.write(img+'\n')
-
+    for img in listdir(dataset_dir + 'valid/' + clas):
+        f.write(img + '\n')
 
 f.close()
 
@@ -23,7 +23,7 @@ f.close()
 f = open(dataset_dir+'test_images.txt')
 
 for img in listdir(dataset_dir + 'test/'):
-    f.write(img+'\n')
-
+    for img in listdir(dataset_dir + 'test/' + clas):
+        f.write(img + '\n')
 
 f.close()
