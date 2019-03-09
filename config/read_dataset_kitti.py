889b51d0906c51ceeadb313e86272725b28ca442
Nilai Sallent <nilaisr@gmail.com>
2019-03-09 21:57:56 +0100

    MOD more kitti 2

--- config/read_dataset_kitti.py
+++ config/read_dataset_kitti.py
@@ -1,18 +1,14 @@
 from os import listdir
 
-dataset_dir = '/home/mcv/datasets/M5/classification/KITTI/'
+dataset_dir = '/home/grupo07/mcv/datasets/M5/classification/KITTI/'
 
-save_dir = '/home/M5/MCV_CNN_framework/'
+save_dir = '/home/grupo07/M5/MCV_CNN_framework/'
 
 f = open(save_dir+'train_images.txt',"w+")
 
-print('pas1')
-
 for clas in listdir(dataset_dir + 'train/'):
-    print('pas2')
     for img in listdir(dataset_dir + 'train/' + clas + '/'):
-        f.write(img+'\n')
-        print('pas3')
+        f.write(dataset_dir + 'train/' + clas + '/' + img+'\n')
 
 f.close()
 
@@ -21,7 +17,7 @@ f = open(save_dir+'valid_images.txt',"w+")
 
 for img in listdir(dataset_dir + 'valid/'):
     for img in listdir(dataset_dir + 'valid/' + clas + '/'):
-        f.write(img + '\n')
+        f.write(dataset_dir + 'train/' + clas + '/' + img + '\n')
 
 f.close()
 
@@ -30,6 +26,35 @@ f = open(save_dir+'test_images.txt',"w+")
 
 for img in listdir(dataset_dir + 'test/'):
     for img in listdir(dataset_dir + 'test/' + clas + '/'):
-        f.write(img + '\n')
+        f.write(dataset_dir + 'train/' + clas + '/' + img + '\n')
+
+f.close()
+
+
+
+
+f = open(save_dir+'train_gt.txt',"w+")
+
+for clas in listdir(dataset_dir + 'train/'):
+    for img in listdir(dataset_dir + 'train/' + clas + '/'):
+        f.write(clas +'\n')
+
+f.close()
+
+
+f = open(save_dir+'valid_gt.txt',"w+")
+
+for img in listdir(dataset_dir + 'valid/'):
+    for img in listdir(dataset_dir + 'valid/' + clas + '/'):
+        f.write(clas +'\n')
+
+f.close()
+
+
+f = open(save_dir+'test_gt.txt',"w+")
+
+for img in listdir(dataset_dir + 'test/'):
+    for img in listdir(dataset_dir + 'test/' + clas + '/'):
+        f.write(clas +'\n')
 
 f.close()
