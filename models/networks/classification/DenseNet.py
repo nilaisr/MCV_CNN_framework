0ff5614d4814509a9dc1e63f24152d6876ddc001
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-03-09 15:27:26 +0100

    ADD all models

--- models/networks/classification/DenseNet.py
+++ models/networks/classification/DenseNet.py
@@ -1,23 +1,23 @@
 import sys
 from torch import nn
-import torchvision.models.alexnet as models
+import torchvision.models.densenet as models
 sys.path.append('../')
 from models.networks.network import Net
 import math
 
-class AlexNet(Net):
+class DenseNet(Net):
 
-    def __init__(self, cf, num_classes=21, pretrained=False, net_name='alexnet'):
+    def __init__(self, cf, num_classes=21, pretrained=False, net_name='densenet'):
         super(AlexNet, self).__init__(cf)
 
         self.url = 'http://datasets.cvc.uab.es/models/pytorch/basic_vgg16.pth'
         self.pretrained = pretrained
         self.net_name = net_name
 
-        self.model = models.alexnet(pretrained=self.pretrained, num_classes=num_classes)
+        self.model = models.densenet201(pretrained=self.pretrained, num_classes=num_classes)
 
         if pretrained:
-            self.model.classifier[6] = nn.Linear(4096, num_classes)
+            self.classifier = nn.Linear(1024, num_classes)
 
     def forward(self, x):
         return self.model.forward(x)
--- models/networks/classification/SqueezeNet.py
+++ models/networks/classification/SqueezeNet.py
@@ -1,23 +1,23 @@
 import sys
 from torch import nn
-import torchvision.models.alexnet as models
+import torchvision.models.squeezenet as models
 sys.path.append('../')
 from models.networks.network import Net
 import math
 
-class AlexNet(Net):
+class SqueezeNet(Net):
 
-    def __init__(self, cf, num_classes=21, pretrained=False, net_name='alexnet'):
+    def __init__(self, cf, num_classes=21, pretrained=False, net_name='squeezenet'):
         super(AlexNet, self).__init__(cf)
 
         self.url = 'http://datasets.cvc.uab.es/models/pytorch/basic_vgg16.pth'
         self.pretrained = pretrained
         self.net_name = net_name
 
-        self.model = models.alexnet(pretrained=self.pretrained, num_classes=num_classes)
+        self.model = models.squeezenet1_1(pretrained=self.pretrained, num_classes=num_classes)
 
         if pretrained:
-            self.model.classifier[6] = nn.Linear(4096, num_classes)
+            self.model.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
 
     def forward(self, x):
         return self.model.forward(x)
