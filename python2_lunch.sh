bd7dcfe284df44a1627d6dde87c67644950b5424
ramon@cvc.uab.es <grupo07@etse-75-51.uab.es>
2019-03-20 10:42:05 +0100

    ADD W3

---
+++ python2_lunch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -n 4 # Number of cores
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -D /home/grupo07/M5/MCV_CNN_framework # working directory
+#SBATCH -t 0-10:05 # Runtime in D-HH:MM
+#SBATCH -p dcc # Partition to submit to
+#SBATCH --mem 20000 # 16GB solicitados.
+#SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
+#SBATCH -o /home/grupo07/logs/segmentation/%x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e /home/grupo07/logs/segmentation/%x_%u_%j.err # File to which STDERR will be written
+sleep 5
+/usr/local/cuda-9.2/samples/bin/x86_64/linux/release/deviceQuery
+nvidia-smi
+cd ~/M5/MCV_CNN_framework
+python3 main.py --silent --exp_name nilaitest --exp_folder test01 --config_file config/SemSeg_sample_fcn8_Camvid.yml
+ 
---
+++ w3m1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -n 4 # Number of cores
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -D /home/grupo07/M5/MCV_CNN_framework # working directory
+#SBATCH -t 0-10:05 # Runtime in D-HH:MM
+#SBATCH --qos masterlow
+#SBATCH --partition mhigh,mlow
+#SBATCH --mem 20000 # 16GB solicitados.
+#SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
+#SBATCH -o /home/grupo07/logs/segmentation/%x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e /home/grupo07/logs/segmentation/%x_%u_%j.err # File to which STDERR will be written
+sleep 5
+/usr/local/cuda-9.2/samples/bin/x86_64/linux/release/deviceQuery
+nvidia-smi
+cd ~/M5/MCV_CNN_framework
+python main.py --silent --exp_name w3m1 --exp_folder ~/Experiments/segmentation/w3m1 --config_file config/SemSeg_sample_fcn8_Camvid.yml
+ 
